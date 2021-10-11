35b0da48bde79fc840b6ba2ba0fb34717fa6c85c
Aritra Bhattacharjee <aritrabhattacharjee321@gmail.com>
2021-10-11 18:32:46 +0530

    FireBase Authentication

--- app/src/main/java/com/example/medlife/Login.java
+++ app/src/main/java/com/example/medlife/Login.java
@@ -1,15 +1,22 @@
 package com.example.medlife;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 /*
 public class Login extends AppCompatActivity {
     Button logind;
@@ -56,23 +63,20 @@ public class Login extends AppCompatActivity {
 public class Login extends AppCompatActivity {
     private Button logind;
     private ImageButton back;
-    private EditText email,password;
-    private TextView  errorM;
-//    private String User = "Aritra";
-//    private String Pass = "12345";
-    Boolean isValid = false;
+    private EditText email, password;
+    private TextView errorM;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // click event listener for Login button
+
         logind = findViewById(R.id.logindashboard);
-         email =  findViewById(R.id.editTextTextEmailAddress);
-         password =  findViewById((R.id.editTextTextPassword));
-         errorM = findViewById(R.id.errorMessage);
-//        String name = email.getText().toString();
-//        String pass = password.getText().toString();
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById((R.id.editTextTextPassword));
+        errorM = findViewById(R.id.errorMessage);
+
         back = findViewById(R.id.imageButton);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,53 +87,40 @@ public class Login extends AppCompatActivity {
 
             }
         });
-//
-        // click event listener for back button, that leads back to the welcome screen
 
         logind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name = email.getText().toString();
-                String pass = password.getText().toString();
+                String emailN = email.getText().toString().trim();
+                String passwords = password.getText().toString().trim();
 
-                if(name.isEmpty() || pass.isEmpty())
-                {
-                    errorM.setText("");
-                    Toast.makeText(Login.this, "All Fields Required", Toast.LENGTH_SHORT).show();
-                    errorM.setText("All Fields are required");
+                if (TextUtils.isEmpty(emailN)) {
+                    email.setError("Email is required");
                 }
-                else{
-                    isValid = validate(name,pass);
-                    if(!isValid)
-                    {
-                        errorM.setText("");
-                        Toast.makeText(Login.this, "Wrong UserName or Password", Toast.LENGTH_SHORT).show();
-                        errorM.setText("Wrong Username or Password");
-
-                    }
-                    else
-                    {
-                        Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                        Intent in  = new Intent(Login.this, DashBoard.class);
-                        startActivity(in);
-                    }
+                else if (TextUtils.isEmpty(passwords)) {
+                    password.setError("Password is required");
+                }
+                else
+                {
+                    mAuth.signInWithEmailAndPassword(emailN,passwords).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                Intent inte = new Intent(Login.this, DashBoard.class);
+                                startActivity(inte);
+                            }
+                        }
+                    });
                 }
 
             }
         });
     }
-
-    private boolean validate(String name1, String password)
-    {
-        /*if(name1.equals(User) && password.equals(Pass))
-        {
-            return true;
-        }*/
-        if(name1.equals("Aritra") && password.equals("1234"))
-        {
-            return true;
-        }
-        return false;
-    }
 }
+
+
+
+
 
